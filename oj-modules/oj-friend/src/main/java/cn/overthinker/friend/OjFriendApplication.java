5fdc45f857664e7120b9043aa16377c8bb03760c
Plthjlsh <2330252836@qq.com>
2026-04-22 22:03:52 +0800

    first commit

---
+++ oj-modules/oj-friend/src/main/java/cn/overthinker/friend/OjFriendApplication.java
@@ -0,0 +1,10 @@
+package cn.overthinker.friend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjFriendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjFriendApplication.class, args);
+    }
+}
---
+++ oj-modules/oj-job/src/main/java/cn/overthinker/job/OjJobApplication.java
@@ -0,0 +1,10 @@
+package cn.overthinker.job;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjJobApplication.class, args);
+    }
+}
