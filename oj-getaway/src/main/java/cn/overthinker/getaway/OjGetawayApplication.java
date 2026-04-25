e4adab520e5b622470b1f3a4e054800de3cb70e9
Plthjlsh <2330252836@qq.com>
2026-04-26 00:27:33 +0800

    [2026-04-26] 引入api网关

---
+++ oj-getaway/src/main/java/cn/overthinker/getaway/OjGetawayApplication.java
@@ -0,0 +1,11 @@
+package cn.overthinker.getaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjGetawayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjGetawayApplication.class, args);
+    }
+}
